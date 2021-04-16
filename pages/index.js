76d23641b4163d3fee8d5ae6041f2520f9a0d702
Baykam Say <47826352+baykamsay@users.noreply.github.com>
2021-04-16 13:57:23 +0200

    Add prettier

--- pages/index.js
+++ pages/index.js
@@ -1,5 +1,5 @@
-import Head from 'next/head'
-import styles from '../styles/Home.module.css'
+import Head from "next/head";
+import styles from "../styles/Home.module.css";
 
 export default function Home() {
   return (
@@ -10,17 +10,22 @@ export default function Home() {
       </Head>
 
       <main className={styles.main}>
-        <h1 className={styles.title}>
-          Fact Fix
-        </h1>
+        <h1 className={styles.title}>Fact Fix</h1>
 
         <p className={styles.description}>
           Analyze news articles and get a trust rating in seconds!
         </p>
 
         <form className={styles.grid}>
-          <input className={styles.description}  style={{flexGrow: "2"}} type="text" placeholder="Enter article URL"/>
-          <button className={styles.description} type="submit">Analyze</button>
+          <input
+            className={styles.description}
+            style={{ flexGrow: "2" }}
+            type="text"
+            placeholder="Enter article URL"
+          />
+          <button className={styles.description} type="submit">
+            Analyze
+          </button>
         </form>
       </main>
 
@@ -35,5 +40,5 @@ export default function Home() {
         </a>
       </footer> */}
     </div>
-  )
+  );
 }
