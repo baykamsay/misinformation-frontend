bcd9b809ca764a6a8cb5ed63e1828e47bb642972
Baykam Say <47826352+baykamsay@users.noreply.github.com>
2021-05-06 16:13:03 +0200

    Add styling for desktop

--- components/result.js
+++ components/result.js
@@ -5,17 +5,21 @@ export default function Result(props) {
     <div className={styles.container}>
       <main>
         <div className={styles.nav}>
-          <button onClick={props.onBack}>Back</button>
-          <span>
-            Trust Rating: <span>92</span>
+          <button className={styles.back} onClick={props.onBack}>
+            Back
+          </button>
+          <span className={styles.rating}>
+            Trust Rating: <span className={styles.grade}>92</span>
           </span>
         </div>
         <h1>
           Covid: Indian delegates in UK for G7 talks self-isolate after Covid
           cases
         </h1>
-        <p>
-          By Francesca Gillett <br /> May 04, 2021
+        <p className={styles.description}>
+          <i>
+            By Francesca Gillett <br /> May 04, 2021
+          </i>
         </p>
         {Object.values(props.data).map((paragraph) => (
           <p>{paragraph}</p>
